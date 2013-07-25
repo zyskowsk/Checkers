870fbbaf279bc60702048ad9ae48552fda58a9ce
Rob Zyskowski <zyskowski.rob@gmail.com>
2013-07-25 11:09:07 -0700

    Board rendering, basic class methods implemented

--- board.rb
+++ board.rb
@@ -1,3 +1,56 @@
+require_relative 'piece'
+
 class Board
+
+  def initialize 
+    @grid = (0...8).map { |row| [" "] * 8 }
+    @colors = [:red, :white]
+    populate_board
+  end
+  
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+  
+  def []=(pos, piece)
+    x, y = [pos]
+    @grid[x][y] = piece
+  end
+  
+  def populate_board
+    add_pieces(@colors.first)
+    add_pieces(@colors.last)
+  end
+  
+  def open?(pos)
+    # does this space contain a Piece object of any color?
+  end
+  
+  def to_s
+    board_string = "-" * 33 + "\n"
+    @grid.each do |row| 
+      board_string <<"| " + row.join(" | ") + " |\n"
+      board_string <<  "-" * 33 + "\n"
+    end
+         
+    board_string
+  end
+  
+  private
+       
+    def add_pieces(color)
+      rows = (color == :red ? (0..2) : (5..7))
+      rows.each do |row| 
+        @grid[row].map!.with_index do |_,col|
+          next Piece.new(color, self, [row, col]) if (col + row).odd?
+          " "
+        end
+      end
+    end
+  
+    def toggle_color
+      @colors == :red ? :black : :red
+    end
   
 end
--- piece.rb
+++ piece.rb
@@ -1,3 +1,29 @@
+require 'colorize'
+
 class Piece
+  attr_reader :king
+  alias_method :king?, :king
+  
+  NORTH, SOUTH = [[-1, -1], [-1, 1]], [[1, 1], [1, -1]]
+  
+  def initialize(color, board, pos)
+    @color, @board, @pos = color, board, pos
+    @directions = nil
+    @king = false
+  end
+  
+  def destroy
+    @board[@pos] = nil
+  end
+  
+  def premote
+    @king = true
+    @directions = NORTH + SOUTH
+  end
+  
+  def to_s
+     return "K".colorize(@color) if king?
+     "O".colorize(@color)   
+  end
   
 end
